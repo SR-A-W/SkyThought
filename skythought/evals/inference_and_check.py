SR-A-W/SkyThought

commit 2aa5847bcfb9e9c1cec8eae063c8a68f0434ebac

[Evals] Fix issue with reading from system prompt in file (#104)
`model_config.system_prompt` can be a string or a `StringInFile` object...Before passing it to the task handler, we need to read the string from the file/ get the underlying `_string` atttribute from StringInFile

skythought/evals/inference_and_check.py
@@ -237,7 +237,7 @@ def generate_responses_for_dataset(
     # Prepare conversations
     conversations = handler.make_conversations(
         remaining_data,
-        model_config.system_prompt,
+        str(model_config.system_prompt),
         model_config.user_template,
         model_config.assistant_prefill,
     )

scripts/response_rewrite.py
@@ -507,7 +507,7 @@ def main():
         variants_dataset, ["fcs", "fcs_plus1", "fcs_reflection"], llm
     )
 
-    system_prompt = ModelConfig.from_model_id(args.target_model).system_prompt
+    system_prompt = str(ModelConfig.from_model_id(args.target_model).system_prompt)
 
     # Generate conversation format for each variant, which can be used in SimPO/DPO/etc.
     fcs_convo = make_preference_conversations(final_dataset, "fcs", system_prompt)

skythought/evals/models/base.py
@@ -28,6 +28,9 @@ class StringInFile(BaseModel):
     def string(self):
         return self._string
 
+    def __str__(self) -> str:
+        return self._string
+
 
 def read_yaml(path: str):
     with open(path, "r") as f:

commit 5fb4f2b5b17150ae0f9020f9a47941d779126e2f

Fix issue with null system prompt after #104 (#107)
# What does this PR do?

Fixes an issue with null system prompt introduced in #104

skythought/evals/inference_and_check.py
@@ -237,7 +237,7 @@ def generate_responses_for_dataset(
     # Prepare conversations
     conversations = handler.make_conversations(
         remaining_data,
-        str(model_config.system_prompt),
+        str(model_config.system_prompt) if model_config.system_prompt else None,
         model_config.user_template,
         model_config.assistant_prefill,
     )